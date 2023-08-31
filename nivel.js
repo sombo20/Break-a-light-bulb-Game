7297f55d2b7aa74d98eb02725f2f0ad408251403
Vicente Sombo <64757262+sombo20@users.noreply.github.com>
2023-08-30 22:26:18 -0400

    Update nivel.js

--- nivel.js
+++ nivel.js
@@ -1,1 +1,26 @@
-
+window.onload = function(){
+				var facil = document.querySelector("#facil")
+				var medio = document.querySelector("#medio")
+				var hard = document.querySelector("#hard")
+				
+				facil.addEventListener("click",function(){
+					var nivel = facil.getAttribute("valor")
+					var nome = prompt("Qual o seu nome");
+					nome == "" ? nome ="Langa" : ""
+					window.location ="start.html?lamp=60&nivel="+nivel+"&tempo=100&nome="+nome;
+				});
+				
+				medio.addEventListener("click",function(){
+					var nivel = medio.getAttribute("valor")
+					var nome = prompt("Qual o seu nome");
+					 nome == "" ? nome ="Langa" : ""
+					window.location ="start.html?lamp=80&nivel="+nivel+"&tempo=30&nome="+nome;
+				});
+				
+				hard.addEventListener("click",function(){
+					var nivel = hard.getAttribute("valor")
+					var nome = prompt("Qual o seu nome");
+					nome == "" ? nome ="Langa" : ""
+					window.location ="start.html?lamp=55&nivel="+nivel+"&tempo=21&nome="+nome;
+				});
+			};
